53bfc79e1da3a25f14528e5ec88306bb8124505c
okan.cetin <okan.cetin@hepsiburada.com>
2016-09-07 01:06:09 +0300

    #000 okan refactor build scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const path = require("path");
+const webpack = require("webpack");
+const glob = require("glob");
 const nodeExternals = require('webpack-node-externals');
 
+const env = process.env.NODE_ENV;
+const isDev = env == "dev";
+const isTest = env == "test";
+const isProd = env == "prod";
 
 const config = {
-    externals: [nodeExternals()],
+    externals: [isTest ? "mocha" : nodeExternals()],
+    entry: isTest ? glob.sync("./src/**/*.spec.ts") : null,
     output: {
         library: 'ReduxTs',
         libraryTarget: 'umd',
@@ -17,12 +23,19 @@ const config = {
             {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
-                loader: 'awesome-typescript-loader'
+                loader: 'awesome-typescript',
+                query: {
+                    declaration: isDev
+                }
             }
         ]
     },
-    progress: true,
-    colors: true,
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ]
 };
 
 module.exports = config;
