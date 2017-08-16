b2fc6ca8c84e9ee6bd257a6f7fbb9d0970675315
okan.cetin <okan.cetin@hepsiburada.com>
2017-08-16 14:38:27 +0300

    #000 okan add object.assign support

--- src/utils/browserPolyfill.ts
+++ src/utils/browserPolyfill.ts
@@ -12,3 +12,29 @@ if (Function.prototype.name === undefined && Object.defineProperty !== undefined
         set: function (value) { }
     });
 }
+
+/*
+ * Polyfill for Object.assign
+ * https://github.com/Microsoft/TypeScript/issues/3429#issuecomment-173213973
+ */
+if (typeof Object.assign != 'function') {
+    Object.assign = function (target: any) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
+    };
+}
