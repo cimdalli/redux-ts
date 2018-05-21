3a3fb746eaa703c840aa7cef5a51dbac7d16cc49
Okan Cetin <okancetin44@gmail.com>
2018-05-21 02:07:58 +0200

    add own props def

--- src/models/redux.model.ts
+++ src/models/redux.model.ts
@@ -2,15 +2,16 @@ import { Dispatch, AnyAction } from 'redux'
 import { ActionCreatorDefinition } from '..'
 
 export interface StateToProps<TState = any> {
-  <T extends { [key: string]: any } = {}>(map: (store: TState) => T): (
-    store: TState,
-  ) => T
+  <T extends { [key: string]: any } = {}, Town = {}>(
+    map: (store: TState, own: Town) => T,
+  ): (store: TState, own: Town) => T
 }
 
 export interface DispatchToProps<
   TDispatchAction extends AnyAction = AnyAction
 > {
-  <T extends { [key: string]: (...params: any[]) => AnyAction }>(map: T): (
-    dispatch: Dispatch<TDispatchAction>,
-  ) => T
+  <T extends { [key: string]: (...params: any[]) => AnyAction }, Town>(
+    map: T,
+    own: Town,
+  ): (dispatch: Dispatch<TDispatchAction>, own: Town) => T
 }
